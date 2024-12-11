1c7364ec2e0e61757578955df71141d77752c01b
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-11 17:39:57 +0500

    task03

---
+++ task03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void func(int n) {
+    if (n < 1) {
+        return; 
+    }
+   
+    func(n - 1);
+    if (n % 2 == 0) {
+        printf("%d ", n);
+    }
+}
+
+int main() {
+   
+    func(9); 
+    printf("\n");
+    func(6); 
+  
+
+    return 0;
+}
